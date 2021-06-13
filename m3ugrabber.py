5d99ae4a918b315db7e6ccdc138bd9e75973370f
benmoose39 <unnik1998@gmail.com>
2021-06-13 18:49:14 +0530

    Add files via upload

---
+++ m3ugrabber.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+import requests
+
+url = 'https://www.youtube.com/watch?v=zcrUCvBD16k'
+response = requests.get(url).text
+
+end = response.find('.m3u8') + 50
+
+tuner = 100
+while True:
+	if 'https://' in response[end-tuner : end]:
+		link = response[end-tuner : end]
+		start = link.find('https://')
+		end = link.find('.m3u8') + 5
+		break
+	else:
+		tuner += 5
+
+
+print(f"{link[start : end]}")
