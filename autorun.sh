b753a3656dadefdd50383463efc8d80d33500df8
benmoose39 <unnik1998@gmail.com>
2021-06-14 20:53:52 +0530

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -2,8 +2,7 @@
 
 python3 -m pip install requests
 
-echo "#EXTM3U" > link.m3u
-echo "#EXTINF:-1 tvg-logo=\"https://www.twentyfournews.com/wp-content/themes/nextline_v3/images/logo-new.png\" tvg-chno=\"1\" group-title=\"Malayalam \(TataSky\)\",24 News Malayalam\" >> link.m3u
+cat info.txt > 24.m3u
 
 chmod +x m3ugrabber.py
 ./m3ugrabber.py >> link.m3u
