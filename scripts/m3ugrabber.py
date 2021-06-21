407266ee5576d1453ed23134e06a9eae74eb0c8b
benmoose39 <unnik1998@gmail.com>
2021-06-21 11:08:47 +0530

    Update m3ugrabber.py

--- scripts/m3ugrabber.py
+++ scripts/m3ugrabber.py
@@ -28,7 +28,7 @@ def grab(url):
             tuner += 5
     print(f"{link[start : end]}")
 
-print("#EXTM3U")
+print('#EXTM3U x-tvg-url="http://indtv.online/guide.gz"')
 print(banner)
 with open('../channel_info.txt') as f:
     for line in f:
