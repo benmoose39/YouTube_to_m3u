f98ab1883a2d8da1f3c805638f065d838d684987
benmoose39 <unnik1998@gmail.com>
2021-06-21 16:36:01 +0530

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -3,11 +3,9 @@
 python3 -m pip install requests
 
 cd scripts
-chmod +x youtube_m3ugrabber.py
-chmod +x ustvgo_m3ugrabber.py
 
-./youtube_m3ugrabber.py > ../youtube.m3u
-./ustvgo_m3ugrabber.py > ../ustvgo.m3u
+python3 youtube_m3ugrabber.py > ../youtube.m3u
+python3 ustvgo_m3ugrabber.py > ../ustvgo.m3u
 
 echo m3u grabbed
 
