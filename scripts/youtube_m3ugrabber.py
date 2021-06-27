13ddadb1b0a4895d6d3a90b0eb8e525585932d1a
nbknri <nbknri@gmail.com>
2021-06-27 16:22:54 +0530

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -29,7 +29,7 @@ def grab(url):
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
+            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/nbknri.m3u8')
             return
     end = response.find('.m3u8') + 5
     tuner = 100
