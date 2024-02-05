a654b2db393bee8a82b69373d539c418fcc2d0b5
Fahmi Fisal <ryuzaki_l@y7mail.com>
2024-02-05 18:30:26 +0800

    update
    fix url

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -26,13 +26,13 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
+                print('https://raw.githubusercontent.com/Justryuz/YouTube_to_m3u/main/youtube.m3u')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
+                print('https://raw.githubusercontent.com/Justryuz/YouTube_to_m3u/main/youtube.m3u')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
